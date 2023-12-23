5d21026540ef742d90b2af5c53089fc696d115e5
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-12-23 08:09:22 +0100

    using Puppet to install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,10 @@
-# Ensure Python and pip are installed
-package { 'python3':
-  ensure => installed,
-}
-
+# Install puppet-lint package
 package { 'python3-pip':
   ensure => installed,
   require => Package['python3'],
 }
 
-# Install Flask version 2.1.0 using pip3
-exec { 'install-flask':
-  command => 'pip3 install flask==2.1.0',
-  unless  => 'pip3 freeze | grep -Fxq Flask==2.1.0',
-  require => Package['python3-pip'],
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'gem',
 }
