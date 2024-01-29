89c030d31b5b587da19653b49a2eed393724e881
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-01-29 22:02:33 +0100

    submit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,42 +5,21 @@ import csv
 import requests
 import sys
 
-base_url = 'https://jsonplaceholder.typicode.com/'
-
-
-def do_request():
-    '''Performs request'''
-
-    if not len(sys.argv):
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
-
-    response = requests.get(base_url + 'users/' + eid)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-
-    with open(eid + '.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, lineterminator='\n',
-                            quoting=csv.QUOTE_ALL)
-        [writer.writerow(['{}'.format(field) for field in
-                          (todo.get('userId'), user.get('username'),
-                           todo.get('completed'), todo.get('title'))])
-         for todo in user_todos]
 
 if __name__ == '__main__':
-    do_request()
+    url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    r = requests.get(url)
+    if r.status_code == 200:
+        username = r.json().get("username")
+        url2 = 'https://jsonplaceholder.typicode.com/todos'
+        r2 = requests.get(url2)
+        filename = sys.argv[1] + '.csv'
+        with open(filename, 'w') as f:
+            wr = csv.writer(f, quoting=csv.QUOTE_ALL, delimiter=',')
+            for item in r2.json():
+                if item.get("userId") == int(sys.argv[1]):
+                    line = [item.get("userId"),
+                            username,
+                            str(item.get("completed")),
+                            item.get('title')]
+                    wr.writerow(line)
