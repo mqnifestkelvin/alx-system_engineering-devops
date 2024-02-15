0f18a586d6e3dd02c1373afb3ac8b66e5869eb26
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-02-15 21:45:08 +0100

    submit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,15 @@
 '''Get number of reddit channel subscribers'''
 import requests
 
-BASE_URL = 'http://reddit.com/r/{}/about.json'
-
 
 def number_of_subscribers(subreddit):
-    '''Gets number of reddit subscribers'''
-    headers = {'User-agent': 'Unix:0-subs:v1'}
-    response = requests.get(BASE_URL.format(subreddit),
-                            headers=headers)
-    if response.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return response.json().get('data', {}).get('subscribers', 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
